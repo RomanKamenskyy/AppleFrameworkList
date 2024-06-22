ce5163b1e2b5e59900a823c6e3055c3e6226bb06
roman <kamensky.ra@gmail.com>
2024-06-22 21:16:37 +0200

    Initial Commit

---
+++ AppleFramework/AppleFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworkApp.swift
+//  AppleFramework
+//
+//  Created by roman on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
